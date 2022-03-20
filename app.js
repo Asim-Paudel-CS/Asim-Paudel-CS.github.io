fa74b8dc6d0e4a76722eb76eff1b6fd3acc09117
Asim Paudel <073bce034.asim@pcampus.edu.np>
2022-03-21 02:12:33 +0545

    Listed Links

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 "use strict"
 //console.log("Test");
+var firstTime = true;
 var slideIndex = 0;
 var linkIndex = 0;
 var linkIndexPrev = 0;
@@ -17,11 +18,14 @@ function showSlides() {
     slides[i].style.display = "none";
     slides[i].style = "z-index : 1";
   }
-  if(linkIndex !== linkIndexPrev){
-    links[linkIndex].style.display = "block";
-    for (i = 0; i < links.length; i++) {
-      if(i != linkIndex){links[i].style.display = "none";}
-    }
+  if (firstTime){
+    links[linkIndex].style.animation = "slidein 6s";
+  }
+  if(linkIndex != linkIndexPrev){
+    links[linkIndex].style.animation = "none";
+    links[linkIndex].offsetHeight;//triggers reflow needs update to run animation twice
+    links[linkIndex].style.animation = "slidein 6s";
+    console.log("Change");
   }
   if (slideIndex > slides.length) {slideIndex = 1}
   if(slideIndex == slides.length){slides[0].style = "z-index : 4";slides[0].style.animation = "popin 2s";}
@@ -33,6 +37,7 @@ function showSlides() {
     slides[slideIndex-1].style.animation = "popout 2s";
      }
   catch{}
+  firstTime = false;
   setTimeout(showSlides, 2000);
-console.log(slideIndex-1);console.log(slides[slideIndex-1]);console.log(linkIndex);
+console.log(slideIndex-1);console.log(slides[slideIndex-1]);console.log(linkIndex);console.log(linkIndexPrev);
 }
